ebee1a4185ccc7e3a874d7c1e2185de8753db62d
Sergij Andrunkiv <sergij.andrunkiv@gmail.com>
2022-01-18 17:04:49 +0200

    added function combine_nps_csvs

--- main.py
+++ main.py
@@ -29,6 +29,30 @@ def check_csv(csv_name):
             return False
 
 
-# Test the function on a corrupted NPS file
+def combine_nps_csvs(csvs_dict):
+    """ Converts all csv_files to DataFrames by adding the source and group columns,
+        and combines them into one DataFrame
+        Args:
+            csvs_dict (dict): The dictionary of pair (csv_file: source_column)
+        Returns:
+            DataFrame: the combined DataFrame """
+
+    combined = pd.DataFrame()
+    for name, source in csvs_dict.items():
+        if check_csv(name) is True:
+            temp = convert_csv_to_df(name, source)
+            combined = pd.concat([combined, temp])
+        else:
+            print(name + " is not a valid file and will not be added.")
+    return combined
+
+
+my_files = {
+  "datasets/2020Q4_nps_email.csv": "email",
+  "datasets/2020Q4_nps_mobile.csv": "mobile",
+  "datasets/2020Q4_nps_web.csv": "web",
+  "datasets/corrupted.csv": "social_media"
+}
+# Test the function on the my_files dictionary
 if __name__ == '__main__':
-    print(check_csv('datasets/corrupted.csv'))
+    print(combine_nps_csvs(my_files))
