c0293427818645a7ebe3314f78103ceb835062c5
Sergij Andrunkiv <sergij.andrunkiv@gmail.com>
2022-01-18 15:50:10 +0200

    created catalog datasets and added function convert_csv_to_df to main.py

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+import pandas as pd
+
+
+def convert_csv_to_df(csv_name, source_type):
+    """ Converts an NPS CSV into a DataFrame with a column for the source.
+    Args:
+        csv_name (str): The name of the NPS CSV file.
+        source_type (str): The source of the NPS responses.
+    Returns:
+        A DataFrame with the CSV data and a column, source. """
+
+    df = pd.read_csv(csv_name)
+    df['source'] = source_type
+    return df
+
+
+# Test the function on the mobile data
 if __name__ == '__main__':
-    print('Hello World')
+    print(convert_csv_to_df("datasets/2020Q4_nps_mobile.csv", "mobile"))
